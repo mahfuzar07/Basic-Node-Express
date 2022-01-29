3d1373ca4611d8ea122b159a0830f017e3e9633a
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2022-01-29 22:09:35 +0600

    OS Module

---
+++ 2_Build-in Modules/2_OS Module/os.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+//node Operating System arch
+console.log(os.arch());
+//node Operating System CPUC
+console.log(os.cpus());
+//node Operating System Host Name
+console.log(os.hostname());
+//node Operating System type
+console.log(os.type());
+//node Operating System User Info
+console.log(os.userInfo());
+//node Operating System User Info
+console.log(os.networkInterfaces());
