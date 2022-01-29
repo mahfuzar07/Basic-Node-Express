0cf4c1df6183a02026142ae5e6a5b2371d221dd5
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2022-01-29 21:15:53 +0600

    local module exports and required

---
+++ Local-Module/index.js
@@ -0,0 +1,5 @@
+const people = require('./people');
+
+console.log(people.getName());
+console.log(people.getAge());
+console.log(people.add(40, 20));
---
+++ Local-Module/people.js
@@ -0,0 +1,15 @@
+const getName = () => {
+  return 'Md. Mahfuzar Rahman';
+};
+const getAge = () => {
+  return 24;
+};
+const add = (a, b) => a + b;
+// exports.getName = getName;
+// exports.getAge = getAge;
+
+module.exports = {
+  getName,
+  getAge,
+  add,
+};
